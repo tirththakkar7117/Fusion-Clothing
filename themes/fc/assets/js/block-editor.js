68d7d076e4b2a1f49ddc19b249d6f67856e624a2
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-02-13 13:15:48 -0500

    Extended Button Block to match the Theme.

--- themes/fc/assets/js/block-editor.js
+++ themes/fc/assets/js/block-editor.js
@@ -7,3 +7,13 @@ wp.domReady( function () {
     wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
     wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
 } );
+
+wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'fc-button',
+    label: 'FC Button',
+} );
+
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
+    wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+} );
