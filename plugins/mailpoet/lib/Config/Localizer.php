af3efc19b47a2108e7d0b79e4ca647cda7a24ffd
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-02-15 19:15:52 -0500

    Woocommerce and Woocommerce Block Plugin Installed and activated as needed for design.

---
+++ plugins/mailpoet/lib/Config/Localizer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MailPoet\Config;
+
+if (!defined('ABSPATH')) exit;
+
+
+use MailPoet\WP\Functions as WPFunctions;
+
+class Localizer {
+  public function init() {
+    $this->loadGlobalText();
+  }
+
+  public function loadGlobalText() {
+    $languagePath = sprintf(
+      '%s/%s-%s.mo',
+      Env::$languagesPath,
+      Env::$pluginName,
+      $this->locale()
+    );
+    WPFunctions::get()->loadTextdomain(Env::$pluginName, $languagePath);
+  }
+
+  public function locale() {
+    $locale = WPFunctions::get()->applyFilters(
+      'plugin_locale',
+      WPFunctions::get()->getUserLocale(),
+      Env::$pluginName
+    );
+    return $locale;
+  }
+
+  public function forceLoadWebsiteLocaleText() {
+    $languagePath = sprintf(
+      '%s/%s-%s.mo',
+      Env::$languagesPath,
+      Env::$pluginName,
+      WPFunctions::get()->getLocale()
+    );
+    WPFunctions::get()->unloadTextdomain(Env::$pluginName);
+    WPFunctions::get()->loadTextdomain(Env::$pluginName, $languagePath);
+  }
+}
