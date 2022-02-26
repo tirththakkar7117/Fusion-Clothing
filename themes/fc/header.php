c8bca8b11f6c720cd8813fd431c40634e3c3b774
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-02-26 14:00:34 -0500

    Created Dynamic Menu

--- themes/fc/header.php
+++ themes/fc/header.php
@@ -30,24 +30,16 @@
 			<a href="http://clothing.local/"><img src="http://clothingdeliverables2.local/wp-content/uploads/2022/02/logo.png" alt="Fusion Clothing Logo" style="width: 200px;" /></a>
 		</div>
 		<!-- .site-branding -->
-		<div class="column-6 center flex-center">
-			<nav id="site-navigation" class="main-navigation">
-				<div id="primary-menu" class="menu">
-					<ul>
-						<li class="page_item page-item-90 current_page_item"><a href="http://clothing.local/" aria-current="page">Home</a></li>
-						<li class="page_item">
-							<a href="http://clothingdeliverables2.local/about/">About</a>
-						</li>
-						<li class="page_item page-item-114">
-							<a href="http://clothingdeliverables2.local/contact-page/">Contact</a>
-						</li>
-						<li class="page_item page-item-5"><a href="http://clothingdeliverables2.local/test-block/">Test Block</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-		<!-- #site-navigation -->
+		<nav id="site-navigation" class="main-navigation">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-primary',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
 		<div class="icons column-3 center flex-no-width">
 		<i class="fa fa-user fa-icon font-custom-style" aria-hidden="true"></i>
             <i class="fa fa-shopping-cart fa-icon font-custom-style" aria-hidden="true"></i>
