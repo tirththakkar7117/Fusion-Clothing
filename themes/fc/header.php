169082220691cfd8a9cab1e04a943cd5a36024e6
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-02-27 15:15:44 -0500

    Changed header logo from static to dynamic.

--- themes/fc/header.php
+++ themes/fc/header.php
@@ -27,7 +27,22 @@
 <header id="masthead" class="site-header">
 	<div class="row">
 		<div class="site-branding column-3 center">
-			<a href="http://clothing.local/"><img src="http://clothingdeliverables2.local/wp-content/uploads/2022/02/logo.png" alt="Fusion Clothing Logo" style="width: 200px;" /></a>
+		<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$fc_description = get_bloginfo( 'description', 'display' );
+			if ( $fc_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $fc_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
 		</div>
 		<!-- .site-branding -->
 		<nav id="site-navigation" class="main-navigation">
