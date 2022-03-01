4dbf0f6b4082ce7aaac8183ffcf9fea970a1d849
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-03-01 17:53:09 -0500

    Removed static footer from sidebar.php and added into footer.php

--- themes/fc/sidebar.php
+++ themes/fc/sidebar.php
@@ -11,29 +11,3 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
-<aside id="secondary" class="widget-area" style="margin:-28px 0 0 0px;">
-	<?php //dynamic_sidebar( 'sidebar-1' ); ?>
-	<div class="row bg-primary-color">
-		<div class="col-4">
-			<h2 class="center">Quick Links</h2>
-			<ul>
-				<li class="center li-style">Home</li>
-				<li class="center li-style">About</li>
-				<li class="center li-style">Contact</li>
-			</ul>
-		</div>
-		<div class="col-4">
-		<h2 class="center">About Us</h2>
-		<p class="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non iaculis risus. Fusce eu felis finibus, dignissim tellus quis, blandit mi. Aliquam ipsum massa, tristique lacinia lacus non, luctus gravida tellus. Praesent facilisis enim eu nibh malesuada, et iaculis elit molestie.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non iaculis risus. Fusce eu felis finibus, dignissim tellus quis, blandit mi. Aliquam ipsum massa, tristique lacinia lacus non, luctus gravida tellus. Praesent facilisis enim eu nibh malesuada, et iaculis elit molestie.</p>
-		</div>
-		<div class="col-4" style="text-align:center;">
-			<h2 class="center">Contact Information</h2>
-			<p class="center">Phone: +1 (123)-456-7890</p>
-			<p class="center">Email: info@domain.com</p>
-			<i class="fa fa-facebook-f white"></i>
-			<i class="fa fa-instagram white center"></i>
-			<i class="fa fa-twitter white center"></i>
-		</div>
-	</div>
-</aside><!-- #secondary -->
