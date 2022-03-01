7ed0b3ca9f7a3beae8a2e2cfc83e782803cc601b
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-03-01 18:21:34 -0500

    Three columns of footer made dynamic except social media.

--- themes/fc/inc/customizer.php
+++ themes/fc/inc/customizer.php
@@ -81,6 +81,80 @@ function fc_customize_register( $wp_customize ) {
 		'section' => 'fc_header_cart'
 	) );
 
+	//* Footer Dynamic 
+	$wp_customize->add_panel( 'fc_footer_items', array (
+		'title'  =>esc_html__( 'Footer Items', 'fc' )
+	) );
+
+	$wp_customize->add_section( 'fc_navigation_title_footer', array (
+		'title'  =>esc_html__( 'Navigation Section', 'fc' ),
+		'panel'  => 'fc_footer_items'
+	) );
+
+	$wp_customize->add_setting( 'fc_nav_title', array () );
+
+	$wp_customize->add_control( 'fc_nav_title', array(
+		'label'   => 'Footer Navigation Title',
+		'type'    => 'text',
+		'description' => 'Enter Title:',
+		'section' => 'fc_navigation_title_footer'
+	) );
+
+	$wp_customize->add_section( 'fc_about_section', array (
+		'title'  =>esc_html__( 'About us Section', 'fc' ),
+		'panel'  => 'fc_footer_items'
+	) );
+
+	$wp_customize->add_setting( 'fc_title', array () );
+
+	$wp_customize->add_control( 'fc_title', array(
+		'label'   => 'Enter Heading for Title.',
+		'type'    => 'text',
+		'description' => 'Enter title For about us content.',
+		'section' => 'fc_about_section'
+	) );
+
+	$wp_customize->add_setting( 'fc_about_content', array () );
+
+	$wp_customize->add_control( 'fc_about_content', array(
+		'label'   => 'Footer About Content',
+		'type'    => 'text',
+		'description' => 'Enter About Us Content for the Footer.',
+		'section' => 'fc_about_section'
+	) );
+
+	$wp_customize->add_section( 'fc_contact_section', array (
+		'title'  =>esc_html__( 'Contact Information Section', 'fc' ),
+		'panel'  => 'fc_footer_items'
+	) );
+
+	$wp_customize->add_setting( 'fc_contact_title', array () );
+
+	$wp_customize->add_control( 'fc_contact_title', array(
+		'label'   => 'Contact Title',
+		'type'    => 'text',
+		'description' => 'Enter Title.',
+		'section' => 'fc_contact_section'
+	) );
+
+	$wp_customize->add_setting( 'fc_phone_information', array () );
+
+	$wp_customize->add_control( 'fc_phone_information', array(
+		'label'   => 'Footer Phone Content',
+		'type'    => 'text',
+		'description' => 'Enter Phone Number:',
+		'section' => 'fc_contact_section'
+	) );
+
+	$wp_customize->add_setting( 'fc_email_information', array () );
+
+	$wp_customize->add_control( 'fc_email_information', array(
+		'label'   => 'Footer Email Content',
+		'type'    => 'text',
+		'description' => 'Enter Email:',
+		'section' => 'fc_contact_section'
+	) );
+
 }
 add_action( 'customize_register', 'fc_customize_register' );
 
