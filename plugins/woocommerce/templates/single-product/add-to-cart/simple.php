4e6f89f99c26dae4538f1b3fb603f39d11993da4
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-03-04 19:39:34 -0500

    Styled the woo-commerce pages as per the design.

--- plugins/woocommerce/templates/single-product/add-to-cart/simple.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/simple.php
@@ -46,9 +46,13 @@ if ( $product->is_in_stock() ) : ?>
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+	</form>
+
+	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
+	<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 	</form>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
