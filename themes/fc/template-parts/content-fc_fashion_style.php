7e89f892097179eddf59615fe43b62d0b4fa6bfd
Tirth Thakkar <tirth.thakkar@dcmail.ca>
2022-03-26 18:57:09 -0400

    Styled the fashion style post type to match the blog of the site.

--- themes/fc/template-parts/content-fc_fashion_style.php
+++ themes/fc/template-parts/content-fc_fashion_style.php
@@ -7,11 +7,60 @@
  * @package Fusion_Clothing
  */
 
+if( ! is_singular( 'fc_fashion_style' ) ) {
+	?>
+	<div class="colm-3">
+    <article id="padding custom-post-bg post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        
+        <?php fc_post_thumbnail(); ?>
+        <?php
+            if ( is_singular() ) :
+                the_title( '<h1 class="entry-title center">', '</h1>' );
+            else :
+                the_title( '<h2 class="entry-title center"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            endif;
 
-echo 'This is the content file for fashion styles.';
-?>
+            if ( 'post' === get_post_type() ) :
+                ?>
+            <?php endif; ?>
 
-<article id="custom-post-bg post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="entry-content width-auto">
+            <?php
+            the_excerpt(
+                sprintf(
+                    wp_kses(
+                        /* translators: %s: Name of current post. Only visible to screen readers */
+                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'fc' ),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    wp_kses_post( get_the_title() )
+                )
+            ); ?>
+            <div class="entry-meta">
+                    
+                </div><!-- .entry-meta -->
+
+                <?php
+            wp_link_pages(
+                array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fc' ),
+                    'after'  => '</div>',
+                )
+            );
+            ?>
+        </div><!-- .entry-content --></div>
+
+        <footer class="entry-footer">
+        </footer><!-- .entry-footer -->
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+	<?php
+} else { ?>
+    <article id="custom-post-bg post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header post-header">
 		<?php
 		if ( is_singular() ) :
@@ -62,4 +111,5 @@ echo 'This is the content file for fashion styles.';
 	<footer class="entry-footer">
 		<?php fc_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
+<?php } ?>
